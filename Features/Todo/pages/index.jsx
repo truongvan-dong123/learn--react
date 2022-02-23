e5ea8876e16eb541e3111225b1b7272fefb81bde
Dong <truongvdong2000@gmail.com>
2022-02-23 17:31:29 +0700

    them

---
+++ Features/Todo/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.scss'
+TodoFeatures.propTypes = {
+    
+};
+
+function TodoFeatures(props) {
+    return (
+        <div>
+            TodoFeatures
+        </div>
+    );
+}
+
+export default TodoFeatures;
--- src/App.js
+++ src/App.js
@@ -2,14 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "dong"
+  const age = "22"
+  const isMale = true;
+  const student = {
+          name: "Johnease FE"
+  }
+  const colorList = ["blue","red"]
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Dong pro
+          Dong pro 123 
         </p>
-       
+       <p>xin chao tôi là {name} {age} {isMale ? "male" : "famale"}  </p>
+
+       {isMale ?<p>male</p> : <p>female</p>}
+
+       {isMale && <p>male</p>}
+       {!isMale && <p>female</p>}
+
+      {isMale && (
+        <div>
+        <p>male 1</p> 
+        <p>male 2</p>
+        <p>male 3</p> 
+        </div>
+      
+      )
+       }
+
+      {isMale && (
+        <>
+        <p>male 1</p>  
+        <p>male 2</p>
+        <p>male 3</p> 
+        </>     
+      )
+       }
+
+      <p>{student.name}</p>
+      <p>{colorList.map(color => <p style ={{color}}>{color}</p> )}</p>
       </header>
     </div>
   );
